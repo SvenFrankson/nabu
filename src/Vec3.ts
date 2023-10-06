61d7f5aed0a0b0a2410cbf90cc943014d0562533
SvenFrankson <sven.frankson@gmail.com>
2023-10-06 17:37:28 +0200

    Add Vec3 Angle and Projection method with ToRef and TmpVec3 cleanup.

--- src/Vec3.ts
+++ src/Vec3.ts
@@ -2,7 +2,53 @@
 
 namespace Nabu {
     
+    var TmpVec3 = [
+        BABYLON.Vector3.Zero(),
+        BABYLON.Vector3.Zero(),
+        BABYLON.Vector3.Zero()
+    ]
+
     export function IsFinite(v: BABYLON.Vector3): boolean {
         return isFinite(v.x) && isFinite(v.y) && isFinite(v.z);
+    }
+
+    export function ProjectPerpendicularAtToRef(v: BABYLON.Vector3, at: BABYLON.Vector3, out: BABYLON.Vector3): BABYLON.Vector3 {
+        let k: number = (v.x * at.x + v.y * at.y + v.z * at.z);
+        k = k / (at.x * at.x + at.y * at.y + at.z * at.z);
+        out.copyFrom(v);
+        out.subtractInPlace(at.multiplyByFloats(k, k, k));
+        return out;
+    }
+
+    export function ProjectPerpendicularAt(v: BABYLON.Vector3, at: BABYLON.Vector3): BABYLON.Vector3 {
+        let p = BABYLON.Vector3.Zero();
+        ProjectPerpendicularAtToRef(v, at, p);
+        return p;
+    }
+
+    export function Angle(from: BABYLON.Vector3, to: BABYLON.Vector3): number {
+        let pFrom = TmpVec3[0];
+        let pTo = TmpVec3[1];
+        pFrom.copyFrom(from).normalize();
+        pTo.copyFrom(from).normalize();
+        let angle: number = Math.acos(BABYLON.Vector3.Dot(pFrom, pTo));
+        return angle;
+    }
+
+    export function AngleFromToAround(from: BABYLON.Vector3, to: BABYLON.Vector3, around: BABYLON.Vector3): number {
+        let pFrom = TmpVec3[0];
+        let pTo = TmpVec3[1];
+        ProjectPerpendicularAtToRef(from, around, pFrom).normalize();
+        ProjectPerpendicularAtToRef(to, around, pTo).normalize();
+        let dot = BABYLON.Vector3.Dot(pFrom, pTo);
+        dot = Math.min(Math.max(dot, -1), 1);
+        let angle: number = Math.acos(dot);
+        if (angle > Math.PI / 360 / 60) {
+            BABYLON.Vector3.CrossToRef(pFrom, pTo, TmpVec3[2]);
+            if (BABYLON.Vector3.Dot(TmpVec3[2], around) < 0) {
+                angle = -angle;
+            }
+        }
+        return angle;
     }
 }
